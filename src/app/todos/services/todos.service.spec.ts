3e9b42c04e393af15cee5d81f73a649a7e406f36
Nadya Chueva <nsbatikova@gmail.com>
2025-01-23 23:18:10 +0100

    test: testing todo service

---
+++ src/app/todos/services/todos.service.spec.ts
@@ -0,0 +1,124 @@
+import { TestBed } from '@angular/core/testing';
+import { TodosService } from './todos.service';
+import { provideHttpClient } from '@angular/common/http';
+import {
+  HttpTestingController,
+  provideHttpClientTesting,
+} from '@angular/common/http/testing';
+import { FilterEnum } from '../types/filter.enum';
+
+describe('TodosService', () => {
+  let todosService: TodosService;
+  let httpTestingController: HttpTestingController;
+  const baseUrl = 'http://localhost:3004/todos';
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        TodosService,
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ],
+    });
+    todosService = TestBed.inject(TodosService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
+  });
+
+  it('creates todo service', () => {
+    expect(todosService).toBeTruthy();
+  });
+
+  it('has initial data', () => {
+    expect(todosService.todosSig()).toEqual([]);
+    expect(todosService.filterSig()).toEqual('all');
+    expect(todosService.apiBaseUrl).toEqual(baseUrl);
+  });
+
+  describe('changeFilter', () => {
+    it('changes filter', () => {
+      todosService.changeFilter(FilterEnum.active);
+      expect(todosService.filterSig()).toEqual(FilterEnum.active);
+    });
+  });
+
+  describe('getTodos', () => {
+    it('gets a todo', () => {
+      todosService.getTodos();
+      const req = httpTestingController.expectOne(baseUrl);
+      req.flush([{ id: '1', text: 'foo', isCompleted: true }]);
+      expect(todosService.todosSig()).toEqual([
+        { id: '1', text: 'foo', isCompleted: true },
+      ]);
+    });
+  });
+
+  describe('addTodo', () => {
+    it('add a todo', () => {
+      todosService.addTodo('foo');
+      const req = httpTestingController.expectOne(baseUrl);
+      req.flush({ id: '1', text: 'foo', isCompleted: false });
+      expect(todosService.todosSig()).toEqual([
+        { id: '1', text: 'foo', isCompleted: false },
+      ]);
+    });
+  });
+  describe('changeTodo', () => {
+    it('change a todo', () => {
+      todosService.todosSig.set([{ id: '1', text: 'foo', isCompleted: false }]);
+      todosService.changeTodo('1', 'bar');
+      const req = httpTestingController.expectOne(`${baseUrl}/1`);
+      req.flush({ id: '1', text: 'bar', isCompleted: false });
+      expect(todosService.todosSig()).toEqual([
+        { id: '1', text: 'bar', isCompleted: false },
+      ]);
+    });
+  });
+  describe('removeTodo', () => {
+    it('remove a todo', () => {
+      todosService.todosSig.set([{ id: '1', text: 'foo', isCompleted: false }]);
+      todosService.removeTodo('1');
+      const req = httpTestingController.expectOne(`${baseUrl}/1`);
+      req.flush({});
+      expect(todosService.todosSig()).toEqual([]);
+    });
+  });
+  describe('toggleTodo', () => {
+    it('toggle a todo', () => {
+      todosService.todosSig.set([
+        { id: '1', text: 'foo', isCompleted: false },
+        { id: '2', text: 'bar', isCompleted: false },
+      ]);
+      todosService.toggleTodo('1');
+      const req = httpTestingController.expectOne(`${baseUrl}/1`);
+      req.flush({ id: '1', text: 'foo', isCompleted: true });
+      expect(todosService.todosSig()).toEqual([
+        { id: '1', text: 'foo', isCompleted: true },
+        { id: '2', text: 'bar', isCompleted: false },
+      ]);
+    });
+  });
+  describe('toggleAll', () => {
+    it('toggle all todos', () => {
+      todosService.todosSig.set([
+        { id: '1', text: 'foo', isCompleted: false },
+        { id: '2', text: 'bar', isCompleted: false },
+      ]);
+      todosService.toggleAll(true);
+
+      const reqs = httpTestingController.match((request) =>
+        request.url.includes(baseUrl)
+      );
+      reqs[0].flush({ id: '1', text: 'foo', isCompleted: true });
+      reqs[1].flush({ id: '2', text: 'bar', isCompleted: true });
+
+      expect(todosService.todosSig()).toEqual([
+        { id: '1', text: 'foo', isCompleted: true },
+        { id: '2', text: 'bar', isCompleted: true },
+      ]);
+    });
+  });
+});
